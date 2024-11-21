40f805704c812dd73a17a5c44f137fef98c0a714
neelyp <160043302+neelyp@users.noreply.github.com>
2024-11-21 11:34:35 -0500

    idek what is new github is tweaking

--- comm.py
+++ comm.py
@@ -15,7 +15,7 @@ def linkBudget(diameter, slantRange):
     # mathy time
     firstSimple = (nr*(((math.pi * diameter) / sol) ** 2)) # simplify the first bunch of parenthisis
     secondSimple = ((4000*math.pi)*slantRange) / sol # simplify second bunch of parenthisis
-    final = (10**((pt+gt-loss+10*math.log(firstSimple,10)-20*math.log(secondSimple,10)-kb-10*math.log(ts,10))/10))/1000
+    final = (10**((pt+gt-loss+10*math.log(firstSimple,10)-20*math.log(secondSimple,10)-kb-10*math.log(ts,10))/10))/1000 # plug in simplified portion into total formula
     return final
 
 print(linkBudget(34,400000))
